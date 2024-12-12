e576ddbdcf2781f84880d2bdb4d64c620efc78f4
michal_efraim <michal.efraim@msmail.ariel.ac.il>
2024-12-12 15:09:55 +0200

    add id

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,5 +1,5 @@
-import java.util.Arrays;
 
+// ID: 324025642
 /**
  * This class represents a simple solution for Ex1.
  * As defined here: https://docs.google.com/document/d/1AJ9wtnL1qdEs4DAKqBlO1bXCM6r6GJ_J/r/edit/edit
